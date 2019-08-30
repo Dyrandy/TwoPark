0c984adb0c279e039050ed269905cc55a36fa6f8
P4rkJW <wr4gcmoima@naver.com>
2019-08-30 16:17:38 +0900

    asdasd

--- tp_frontend/src/App.vue
+++ tp_frontend/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <mainHeader></mainHeader>
     <boardList></boardList>
+    <register></register>
     <mainFooter></mainFooter>
   </div>
 </template>
@@ -10,12 +11,14 @@
   import mainHeader from './components/Header.vue';
   import mainFooter from './components/Footer.vue';
   import boardList from './components/board/boardList.vue';
+  import register from './components/register/register.vue';
 
 export default {
   components: {
       'mainHeader': mainHeader,
       'mainFooter': mainFooter,
       'boardList': boardList
+      'register' : register
   }
 }
 </script>
---
+++ tp_frontend/src/components/register/register.vue
@@ -0,0 +1,138 @@
+<template>
+  <div align="center" id="title" v-bind:style="styleVue">
+    {{ title }}
+  </div>
+  <hr width="500px">
+  <div class="register-page">
+    <div class="form" id="check">
+      <input type="text" v-model="id" placeholder="ID">
+      <input type="text" v-model="nickname" placeholder="NickName">
+      <input type="email" v-model="email" placeholder="Email">
+      <input type="password" v-model="pw"placeholder="Password">
+      <input type="password" v-model="repw" placeholder="Confirm Password">
+      <button v-on:click="check">Register</button>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name : 'register'
+    }
+    // Title Tag
+    var title_style = new Vue({
+        el : '#title',
+        data : {
+            title : 'TwoPark Register!',
+            styleVue : {
+                fontSize : '30px',
+                fontWeight : 'bold',
+                color : 'blue',
+                marginTop : '100px',
+                marginBottom : '10px'
+            }
+        }
+    })
+
+    // Login Check - NULL Value Request Protect
+    var login_check = new Vue({
+        el : '#check',
+        data : {
+            id : '',
+            nickname : '',
+            email : '',
+            pw : '',
+            repw : ''
+        },
+        methods :{
+            check: function () {
+                if(this.id == '') {
+                    alert('Input Your ID')
+                }
+                else if(this.nickname == '') {
+                    alert('Input Your NickName')
+                }
+                else if(this.email == '') {
+                    alert('Input Your Email')
+                }
+                else if(this.pw == '') {
+                    alert('Input Your Password')
+                }
+                else if(this.repw == '') {
+                    alert('Input Your Confirm Password')
+                }
+                else if(this.repw != this.pw) {
+                    alert('NOT Match Your Password and Confirm Password')
+                }
+                else{
+                    ajax.send(this.id, this.nickname, this.email, this.pw, this.repw);
+                }
+            }
+        }
+    })
+
+    // AJAX CODE using Vue-axios - send Register_proc
+    var ajax = new Vue({
+        methods : {
+            send: function(id, nick, email, pw, repw){
+                axios.post('/register_proc',{ id : id, nickname : nick, email : email, pw : pw, repw: repw})
+                    .then(function(res){
+                        console.log(res);
+                    }).catch(function (e) {
+                    console.error(e);
+                })
+            }
+        }
+    })
+</script>
+
+<style scoped>
+  @import url(https://fonts.googleapis.com/css?family=Roboto:300);
+
+  .register-page {
+    width: 360px;
+    padding: 3% 0 0;
+    margin: auto;
+  }
+  .form {
+    position: relative;
+    z-index: 1;
+    background: #FFFFFF;
+    max-width: 360px;
+    margin: 0px 0px 0px 0px;
+    padding: 45px;
+    text-align: center;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+  }
+  .form input {
+    font-family: "Roboto", sans-serif;
+    outline: 0;
+    background: #f2f2f2;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;
+  }
+  .form button {
+    font-family: "Roboto", sans-serif;
+    text-transform: uppercase;
+    outline: 0;
+    background: #4CAF50;
+    width: 100%;
+    border: 0;
+    padding: 15px;
+    color: #FFFFFF;
+    font-size: 14px;
+    -webkit-transition: all 0.3 ease;
+    transition: all 0.3 ease;
+    cursor: pointer;
+  }
+  .form button:hover,.form button:active,.form button:focus {
+    background: #43A047;
+  }
+  .form .register-form {
+    display: none;
+  }
+</style>
