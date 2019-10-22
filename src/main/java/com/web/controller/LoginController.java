229d68ec32b6079a2157c01f2957684760207eb7
caindy <akek1200@gmail.com>
2019-10-22 16:09:34 +0900

    modified index.html added index.css incomplete login, etc

---
+++ src/main/java/com/web/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.web.controller;
+
+import com.web.service.BoardService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+}
